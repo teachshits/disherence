e2b613689cf9e943bc62a19f9a05c49fd0174b4e
BBQ <iny@nm.ru>
2012-07-19 13:25:08 +0400

    js map

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,18 +4,11 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.limit(20)
     @restaurants = @restaurants.by_distance(cookies[:lat], cookies[:lng]) if cookies[:lat] && cookies[:lng]
     
-    @restaurants_info = ""
-    i = 0
+    @restaurants_info = {}
     @restaurants.each do |r|
-      id = r.id
-      name = r.name.gsub(/'/, "\\\\'")
-      lat = r.lat
-      lng = r.lng 
-      @restaurants_info += "r_info = [];"
-      @restaurants_info += "r_info[#{i}][1] = '#{name}';"
-      @restaurants_info += "r_info[#{i}][2] = '#{lat}';"
-      @restaurants_info += "r_info[#{i}][3] = '#{lng}';"
-      i += 1
+      @restaurants_info["r_#{r.id}".to_sym][:name] => r.name.gsub(/'/, "\\\\'")
+      @restaurants_info["r_#{r.id}".to_sym][:lat] => r.lat
+      @restaurants_info["r_#{r.id}".to_sym][:lng] => r.lon
     end
   end
   
