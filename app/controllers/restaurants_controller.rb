0f39356fa8b3b8e8a81fccf3d3424c3c44a3eaac
BBQ <iny@nm.ru>
2012-07-19 11:39:52 +0400

    js popup

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,20 @@
 class RestaurantsController < ApplicationController
   
   def index
-    @restaurant = Restaurant.limit(20)
-    @restaurant = @restaurant.by_distance(cookies[:lat], cookies[:lng]) if cookies[:lat] && cookies[:lng]
+    @restaurants = Restaurant.limit(20)
+    @restaurants = @restaurants.by_distance(cookies[:lat], cookies[:lng]) if cookies[:lat] && cookies[:lng]
+    
+    @restaurants_info = ""
+    @restaurants.each do |r|
+      id = r.dish.restaurant.id
+      name = r.dish.restaurant.name.gsub(/'/, "\\\\'")
+      lat = r.dish.restaurant.lat
+      lng = r.dish.restaurant.lng 
+      @restaurants_info += "r_info['r_#{id}'] = {};"
+      @restaurants_info += "r_info['r_#{id}']['name'] = '#{name}';"
+      @restaurants_info += "r_info['r_#{id}']['lat'] = '#{lat}';"
+      @restaurants_info += "r_info['r_#{id}']['lng'] = '#{lng}';"
+    end
   end
   
   def show
