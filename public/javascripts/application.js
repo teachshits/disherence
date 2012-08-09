c9d1e7b7a10ecc8d0f2adb5a8260f6b6eeccf578
BBQ <iny@nm.ru>
2012-08-09 18:55:13 +0400

    pre beta

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
 		document.ontouchmove = function(e) {e.preventDefault()}
 		$('.users').children('.profiles').each(function(index, item){ item.ontouchmove = function(e) {e.stopPropagation()} })
 		
-		var myScroll
+		myScroll
 		var flag = true
 		var page = 1
 		v_height = document.getElementById('wrapper').scrollHeight
