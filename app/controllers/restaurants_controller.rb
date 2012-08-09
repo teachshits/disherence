13a4393f296f806e95208cd925997dbb3cb00a19
BBQ <iny@nm.ru>
2012-08-09 16:28:43 +0400

    pre beta

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,14 @@ class RestaurantsController < ApplicationController
       @restaurants_info[r.id][:lat] = r.lat
       @restaurants_info[r.id][:lng] = r.lng
     end
+    
+    @restaurants_info = ""
+    @restaurants.each do |r|
+      @restaurants_info += "r_info['r_#{r.id}'] = {};"
+      @restaurants_info += "r_info['r_#{r.id}']['name'] = '#{r.name}';"
+      @restaurants_info += "r_info['r_#{r.id}']['lat'] = '#{r.lat}';"
+      @restaurants_info += "r_info['r_#{r.id}']['lng'] = '#{r.lng}';"
+    end  
   end
   
   def show
