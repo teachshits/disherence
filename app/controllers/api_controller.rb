bd7514c7dbbaa7efc3f23c753656d6e4800bd9e3
BBQ <iny@nm.ru>
2012-08-30 18:17:21 +0400

    api rest

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,17 +1,14 @@
 class ApiController < ApplicationController
   
   def get_res
-    # if params[:lat] && params[:lng]
+    if params[:lat] && params[:lng] 
+      return render :json => Restaurant.by_distance(params[:lat], params[:lng]).as_json
       
+    else
       return render :json => {
-            :restaurants => Restaurant.by_distance(params[:lat], params[:lng]).as_json
+            :error => "lat or/and lng not specified"
       }
-      
-    # else
-      # return render :json => {
-      #       :error => "lat or/and lng not specified"
-      # }
-    # end
+    end
   end
   
   
