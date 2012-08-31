c239da0bbf54c802c090810074b301ee0bafe4c9
BBQ <iny@nm.ru>
2012-08-31 14:24:04 +0400

    api rest

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,16 +1,31 @@
 class ApiController < ApplicationController
   
-  def get_res
-    if params[:lat] && params[:lng]
+  def get_restaurants
+    params[:limit] ||= 25
+    params[:offset] ||= 0
+    
+    unless params[:lat] && params[:lng]
+      params[:lat] = 40.7
+      params[:lng] = -74
+    end
+    
+    data = Restaurant.by_distance(params[:lat], params[:lng]).limit(params[:limit]).offset(params[:offset])
+    
+    return render :json => {
+      :restaurants => data.as_json
+    }
+  end
+  
+  def get_best_dishes
+    if params[:restaurant_id]
+      data = Dish.where(:restaurant_id => params[:restaurant_id]).order("likes - dislikes DESC")
+      
       return render :json => {
-            :restaurants => Restaurant.by_distance(params[:lat], params[:lng]).limit(params[:limit]).offset(params[:offset]).as_json
-      }
-    else
-      return render :json => {
-            :error => "lat or/and lng not specified"
+        :best_dishes => data.as_json
       }
     end
   end
+  
   
   
 end
