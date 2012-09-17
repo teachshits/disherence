25cde057c72c54b8cdd9b7524955e873986a42b0
BBQ <iny@nm.ru>
2012-09-17 14:26:30 +0400

    fb registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
   def user_picture
     if session[:user]
       raw "<div class=\"user_img\" style=\"background: url('#{session[:user].photo}') left center\"></div>"
+    else
+      raw session
     end
   end
   
