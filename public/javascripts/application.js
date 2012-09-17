e849130484253632c0788ebdc4c4bc6eccf4414b
BBQ <iny@nm.ru>
2012-09-17 21:46:51 +0400

    userpic

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -300,11 +300,10 @@ $(document).ready(function() {
 			if (json.url) {
 				window.location.href = json.url
 			} else {
-				el = $(this).parent('.btn_container').prevAll('.stats')
-				
+				el = $(this).parent('.btn_container').prevAll('stats')
 				if (this.className == 'btn_agree_aw') {
 					$(this).prev('.status_aw').addClass('set_agree_aw')
-					el.children('.like').text(json.likes)
+					el.children('.like').text(parseInt(el.children('.like').text()) + 1)
 
 					// if (el.find('.user_info .opinion').text().indexOf('Awesome') != -1) {
 					// 	el.find('.dish_info .likes').text(parseInt(el.find('.dish_info .likes').text()) + 1)
@@ -312,7 +311,9 @@ $(document).ready(function() {
 
 				} else {
 					$(this).prev().prev('.status_aw').addClass('set_disagree_aw')
-					el.children('.dislike').text(json.dislikes)
+					el.children('.dislike').text(parseInt(el.children('.dislike').text()) + 1)
+					
+					// el.find('.disagree').text('#' + (parseInt(el.find('.disagree').text().replace(/\D/g, '')) + 1) + ' user(s) disagree')
 
 					// if (el.find('.user_info .opinion').text().indexOf('Awful') != -1) {
 					// 	el.find('.dish_info .likes').text(parseInt(el.find('.dish_info .likes').text()) + 1)
