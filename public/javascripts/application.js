8f2e91cb725166e35a3bf0620cfab9a6e748d6ca
BBQ <iny@nm.ru>
2012-09-19 14:15:30 +0400

    buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -115,9 +115,9 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('#search_map_canvas').removeClass('expand_search_map_canvas')
 		$(this).addClass('pressed')
-		$("#user_img_profile").removeClass('hidden')
 		setTimeout(function(){ $(".close_map").addClass('hidden').removeClass('pressed') },50);
 		$('#search_on_map').addClass('hidden')
+		$("#user_img_profile").removeClass('hidden')
 	})
 	
 	if ($("#map_canvas").length > 0){
