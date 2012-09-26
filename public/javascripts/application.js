b3950a9c08dd85b4b48dbea80a3970f391482c84
BBQ <iny@nm.ru>
2012-09-26 15:35:30 +0400

    api fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
 		
 		if (($(".review").index(element) - 1)%3 == 0) {
 			console.log(2)
-			element.addClass('slide_2')
+			element.toggleClass('slide_2')
 		} else if (($(".review").index(element) - 2)%3 == 0) {
 			console.log(3)
-			element.addClass('slide_3')
+			element.toggleClass('slide_3')
 		}
 
 		$(this).children('.description').toggleClass('show')
