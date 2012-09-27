432f9b2b55876496b8316d02545c4dd245504e56
Andrey <disco_robot@Andreys-MacBook-Air.local>
2012-09-27 21:11:44 +0400

    js start

--- app/models/review.rb
+++ app/models/review.rb
@@ -32,9 +32,9 @@ class Review < ActiveRecord::Base
   #   super(:only => [:id, :opinion, :comment, :remote_photo], :include => [:user => {:only => [:name, :id, :remote_photo]}], :include => [:dish => {:only => [:name, :id], :include => [:restaurant => {:only => [:name, :address, :id]}]}])
   # end
   
-  def opinion
-    self[:opinion] || 3
-  end
+  # def opinion
+  #   self[:opinion] || 3
+  # end
   
   def photo
     if self[:photo].blank? 
