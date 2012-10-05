a3a714140c8e2c65f3ead68814f469dde0eb8c74
BBQ <iny@nm.ru>
2012-10-05 14:55:51 +0400

    twitter share

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,12 @@ class User < ActiveRecord::Base
     if user = find_by_token(user_token)
       if restaurant = Restaurant.find_by_id(restaurant_id) 
       
-        restaurant_name = CGI.escape(restaurant.name).gsub("+", "%20")
+        # restaurant_name = CGI.escape(restaurant.name).gsub("+", "%20")
         restaurant_address = CGI.escape(restaurant.address).gsub("+", "%20")
         
         if !user.oauth_token_secret.blank? && !user.oauth_token.blank?
           client = Twitter::Client.new(:oauth_token => user.oauth_token, :oauth_token_secret => user.oauth_token_secret)
-          caption = "Check out #{restaurant_name} on @disherence " + CGI.escape("http://demo.disherence.com/restaurants/show/#{restaurant_id}").gsub("+", " ")
+          caption = "Check out #{restaurant.name} on @disherence http://demo.disherence.com/restaurants/show/#{restaurant_id}"
           client.update(caption)
         end
         
