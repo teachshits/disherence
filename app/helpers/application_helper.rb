d4fa43c08d173c76eb8bf6b83c0044016de1d199
BBQ <iny@nm.ru>
2012-10-11 13:02:45 +0400

    fb actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,31 @@ module ApplicationHelper
     end
   end
   
+  def fb_opengraph_meta_tag
+    if params[:id]
+      
+      domain = "http://demo.disherence.com"
+      app_id = '361774547226492'
+      
+      # url = "#{domain}#{eval "#{type}_path(#{@fb_obj.id})" }"
+      
+      title = "Restaurant"
+      url = "#{domain}/restaurants/#{params[:id]}"
+            
+      dish = Dish.where("restaurant_id = ? AND photos > 0 ",params[:id]).first
+      review = Review.where("dish_id = ? AND remote_photo IS NOT NULL", dish.id).first
+      
+      image = review.remote_photo
+      
+      raw %Q{
+        <meta property="fb:app_id" content="#{app_id}" /> 
+        <meta property="og:type"   content="object" /> 
+        <meta property="og:url"    content="#{url}" /> 
+        <meta property="og:title"  content="#{title}" /> 
+        <meta property="og:image"  content="#{image}" />
+      }
+    end
+  end
+  
   
 end
