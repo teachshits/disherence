ca009de1cff1d0559af313d8b24ab50634f00939
BBQ <iny@nm.ru>
2012-10-26 19:28:02 +0400

    profile fix

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,10 +1,19 @@
 module RestaurantsHelper
   
   def remoteness(restaurant, lat_cur, lng_cur)
+    lat_cur = lat_cur.to_f
+    lng_cur = lng_cur.to_f
     
     if restaurant.lat && restaurant.lng && lat_cur && lng_cur
+      
       rem = restaurant.remoteness(lat_cur,lng_cur)
-      rem = rem[:m] < 1000 ? "#{rem[:m].round} m" : "#{rem[:km].round} Km"
+      
+      if lat_cur > 25.64153 && lat_cur < 49.61071 && lng_cur > -125.77148 && lng_cur < -66.09375
+        "#{rem[:mi].round(2)} Mi"
+      else
+        rem[:m] < 1000 ? "#{rem[:m].round} m" : "#{rem[:km].round} Km"
+      end
+      
     else
       'Unknown'
     end
