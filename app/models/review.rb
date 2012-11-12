a567fef3e82a40e7ede47e9c4fc1f4dc0d4f75ac
BBQ <iny@nm.ru>
2012-11-12 16:26:19 +0400

    photo fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -97,12 +97,15 @@ class Review < ActiveRecord::Base
     if rd = find_by_dish_id_and_user_id(dish_id,user_id)
       rd.destroy
     end
+    
     loves_fb_action(User.find_by_id(user_id), dish_id)
     create(:dish_id => dish_id, :user_id => user_id, :opinion => true, :local_photo => local_photo)
+    
     unless local_photo.nil?
       dish = Dish.find_by_id(dish_id)
       dish.update_attributes(:photos => dish.photos + 1)
     end
+    
   end
   
   def self.awful(dish_id, user_id, photo = nil)
